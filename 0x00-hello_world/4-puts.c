fc2fd559bd23299f069f72a1a0548b1b97a9f442
Anteneh Tesfaye <anttesdev@gmail.com>
2023-07-13 12:13:51 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*Main function Entry point 
-*return 0
+* main - description of a programming function
+* Description : prints how programming is like a multilingual puzzle
+* Return: (0) success
 */
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
