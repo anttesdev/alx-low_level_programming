ccce6d4b936b7b37d92aedf38f76992d04be3bce
Anteneh Tesfaye <anttesdev@gmail.com>
2023-07-13 12:51:49 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - This sis a function that prints the text below
+* Return: (1) return is a sucess
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
