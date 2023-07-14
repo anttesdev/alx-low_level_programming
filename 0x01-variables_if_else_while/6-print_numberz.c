e56e02728fc21e098f0a5604ea9c4850fab7f8b1
Anteneh Tesfaye <anttesdev@gmail.com>
2023-07-14 08:27:25 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - entry point of function
+  * Description: a function that prints numbers 0 to 9
+  * Return: (0) is a sucess
+  */
+
+int main(void)
+{
+
+	int i = 48;
+
+	while (i < 58)
+{	putchar(i);
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
