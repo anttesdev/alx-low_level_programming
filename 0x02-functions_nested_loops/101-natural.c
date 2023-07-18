19e3df04ab61e93f6842af5569946e236715a14e
Anteneh Tesfaye <anttesdev@gmail.com>
2023-07-18 12:12:25 -0700

    sum of 5 and 3 multiples to 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Description: a function that sums up 5 multiples and 3 multtiples
+  * Return: 0 is a sucess
+  */
+
+int main(void)
+{
+	int x;
+
+	int sum = 0;
+
+	for (x = 0; x < 1024; x++)
+	{
+	if ((x % 3) == 0 || (x % 5) == 0)
+
+		sum += x;
+	}
+
+	printf("%d\n", sum);
+}
+
+
