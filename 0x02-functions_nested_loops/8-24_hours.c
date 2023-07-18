354b0e1d2fd9638a2cc8dfebe78abad0a48f666d
Anteneh Tesfaye <anttesdev@gmail.com>
2023-07-18 06:02:20 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints out every minute of every hour
+ * Return: 0 sucess
+ */
+
+void jack_bauer(void)
+{
+	int x;
+
+	for (x = 0; x < 25; x++)
+	{
+		int y;
+
+		for (y = 0; y < 60 ; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
