669e689931ce2458e3a6e617d2b57ddad8c2ab45
Anteneh Tesfaye <anttesdev@gmail.com>
2023-07-20 06:32:43 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,32 +9,32 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i <= 100; i++)
 
 	if (i % 3 == 0 && i % 5 != 0)
 	{
-		printf("Fizz ");
+		printf(" Fizz");
 	}
 
 	else if (i % 5 == 0 && i % 3 !=0)
 	{
-		printf("Buzz ");
+		printf(" Buzz ");
 	}
 
 	else if (i % 3 == 0 && i % 5 == 0)
 	{
-		printf("FizzBuzz ");
+		printf(" FizzBuzz ");
 	}
 	else
 	{
-		if ( i < 100)
+		if ( i < 2)
 		{
-		printf("%d ", i);
+		printf("%d", i);
 		}
 
 		else 
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
 
