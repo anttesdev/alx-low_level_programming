ea33397df75ae57a3a141b7d84b760e7a45f6820
Anteneh Tesfaye <anttesdev@gmail.com>
2023-07-24 06:45:24 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,21 +7,19 @@
 
 void rev_string(char *s)
 {
-	char back = s[0];
-	int length = 0;
+	char rev = s[0];
+	int counter = 0;
 	int i;
 
-	while (s[length != '\0'])
-		length++;
-
-	for (i = 0; i < length; i++)
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
 	{
-		length--;
-		back = s[i];
-		s[i] = length;
-		s[length] = back;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
 }
-
 
 
