bb16a8ee2e495c87bc873fdd0de3268f0bdd48d7
Anteneh Tesfaye <anttesdev@gmail.com>
2023-07-27 09:55:30 -0700

    capitalize all words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * cap_string - changes all uppercase elements of string to lowercase
+ * @p: pointer input
+ * Return: lowercase string p
+ */
+
+char *cap_string(char *p)
+{
+	int i;
+
+	i = 0;
+
+	while (p[i])
+	{
+	while (!(p[i] >= 97 && p[i] <= 122))
+	{	
+		i++;
+	}
+		if
+		(p[i - 1] == ' ' ||
+		p[i - 1] == '\t' ||
+		p[i - 1] == '\n' ||
+		p[i - 1] == ',' ||
+		p[i - 1] == ';' ||
+		p[i - 1] == '.' ||
+		p[i - 1] == '!' ||
+		p[i - 1] == '?' ||
+		p[i - 1] == '"' ||
+		p[i - 1] == '(' ||
+		p[i - 1] == ')' ||
+		p[i - 1] == '}' ||
+		p[i - 1] == '{' ||
+		i == 0)
+
+			{
+				p[i] = p[i] - 32;
+			}
+
+		i++;
+	}
+
+	return (p);
+}
