0f539c954965793be3bc454eaddd38377f21305f
Anteneh Tesfaye <anttesdev@gmail.com>
2023-08-08 07:10:33 -0700

    duplicate string after allocation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a new string which is a duplicate of the string str
- * @str: srtring to be duplicated
+ * _strdup - returns a pointer to a new string which is a duplicate of str
+ * @str: string to be duplicated
  * Return pointer to the new string
  */
 
@@ -13,10 +13,10 @@ char *_strdup(char *str)
 	int x;
 
 	s = malloc(sizeof(str) + 1);
-	
+
 	if (str == NULL)
 		return (NULL);
-	
+
 	for (x = 0; x < str[x]; x++)
 	{
 		s[x] = str[x];
