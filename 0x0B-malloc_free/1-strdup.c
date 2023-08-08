95155921860735eb8c1e3e2a0726f75b2cc1e210
Anteneh Tesfaye <anttesdev@gmail.com>
2023-08-08 07:17:24 -0700

    allocate memory for a copied string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,22 +4,28 @@
 /**
  * _strdup - returns a pointer to a new string which is a duplicate of str
  * @str: string to be duplicated
- * Return pointer to the new string
+ * Return: pointer to the new string
  */
 
 char *_strdup(char *str)
 {
 	char *s;
 	int x;
+	int y;
 
-	s = malloc(sizeof(str) + 1);
+	for (x = 0; str[x] != '\0';)
+	{
+		x++;
+	}
+
+	s = malloc(sizeof(char) * (x + 1));
 
 	if (str == NULL)
 		return (NULL);
 
-	for (x = 0; x < str[x]; x++)
+	for (y = 0; y < str[y]; y++)
 	{
-		s[x] = str[x];
+		s[y] = str[y];
 	}
 
 	return (s);
