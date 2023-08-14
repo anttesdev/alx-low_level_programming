44ddd8387a516bbf0de1134f62552e66fa6b8359
Anteneh Tesfaye <anttesdev@gmail.com>
2023-08-14 08:01:44 -0700

    print out the name of the program

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the name of the file it was compiled from
+ * Return: 0 is sucess
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
