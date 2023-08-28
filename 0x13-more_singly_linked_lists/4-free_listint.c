f18b2d703d25c035066e65067df91702a17e74d1
Anteneh Tesfaye <anttesdev@gmail.com>
2023-08-28 10:31:02 -0700

    5

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
  * free_listint - a function that frees a listint_t list
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
  * free_listint2 - a function that frees a listint_t list
