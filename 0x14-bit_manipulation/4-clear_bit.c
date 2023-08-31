9914a1f7ba80c90201aa6aa07b2eba5af001c053
Anteneh Tesfaye <anttesdev@gmail.com>
2023-08-31 12:33:35 -0700

    set bit to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,8 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > 63)
 		return (-1);
 
-	if (((*n) != 0) & ((1 << index) != 0))
+	if ((*n) & (1 << index))
 		*n ^= (1 << index);
-
 	return (1);
 }
