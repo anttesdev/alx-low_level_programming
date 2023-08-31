90d3d3a21f8ca6eb2977a2ac8ab9aa2c79f62f39
Anteneh Tesfaye <anttesdev@gmail.com>
2023-08-31 08:41:13 -0700

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to a positive decimal number
+ * @b: string that stores the binary number in 0 and 1
+ * Return: number or 0 if it is NUll or not made up of 0 and 1
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int decimal = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' || b[i] != '1')
+			return (0);
+
+		decimal = decimal << 1;
+		decimal |= (b[i] - '0');
+	}
+
+	return (decimal);
+}
