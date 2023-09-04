f0943bdda6ea7ed1e3214bd78ca48a08d88e9c5f
Anteneh Tesfaye <anttesdev@gmail.com>
2023-09-04 14:03:25 -0700

    copy file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,108 @@
+#include "main.h"
+
+/**
+ * check_read - checks whether a file can be opened and read
+ * @file_from: the file the content to be copied from
+ * @argv: array of arguments
+ * Return: Nothing
+ */
+
+void check_read(int file_from, char *argv[])
+{
+	if (file_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+}
+
+
+/**
+ * check_write - checks whether a file can be opened and read
+ * @file_to: the file the content is copied to
+ * @argv: array of arguments
+ * Return: Nothing
+ */
+void check_write(int file_to, char *argv[])
+{
+	if (file_to < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * close_f - Closes a file descriptors
+ * @file: the file
+ */
+
+void close_f(int file)
+{
+	int cl;
+
+	cl = close(file);
+
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", file);
+		exit(100);
+	}
+}
+
+
+/**
+ * main - a program that copies the content of a file to another file
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: 0 is success
+ */
+
+int main(int argc, char *argv[])
+{
+	int from, to, re, wr;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
+	}
+	from = open(argv[1], O_RDONLY);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+		check_write(to, argv);
+
+	re = read(from, buffer, 1024);
+
+	while (re > 0)
+	{
+		if (from == -1 || re == -1)
+		{
+			free(buffer);
+			check_read(from, argv);
+		}
+		wr = write(to, buffer, re);
+		if (to == -1 || wr == -1)
+		{
+			free(buffer);
+			check_write(to, argv);
+		}
+		to = open(argv[2], O_WRONLY | O_APPEND);
+	}
+	free(buffer);
+	close_f(from);
+	close_f(to);
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
