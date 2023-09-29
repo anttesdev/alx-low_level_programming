517bcaa714237303d486d3d6b54d7957e23529c3
Anteneh <anttesdev@gmail.com>
2023-09-29 07:17:54 -0700

    set value to 1 the bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int selectbit = 1UL << index;
 
 	if (index >= (sizeof(unsigned long int) * 8))
-			return (-1);
+		return (-1);
 
 	*n = *n | selectbit;
 
