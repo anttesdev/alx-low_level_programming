c5df6340c89ef06d37e55a7545c6be95375a662f
Anteneh <anttesdev@gmail.com>
2023-10-02 13:19:02 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,28 +44,31 @@ int main(int argc, char **argv)
 	}
 
 	src = open(argv[1], O_RDONLY);
-	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR |
+		S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
 	handle_error(src, dest, argv);
 
-	char_count = BUFFER;
-
-	while (char_count == 1024)
+	while ((char_count = read(src, buff, BUFFER)) > 0)
 	{
-		char_count = read(src, buff, 1024);
-		if (char_count == -1)
-			handle_error(-1, 0, argv);
 		wr = write(dest, buff, char_count);
 		if (wr == -1)
-			handle_error(0, -1, argv);
-	}
+			handle_error(src, dest, argv);
 
+	}
+	if (char_count == -1)
+		handle_error(src, dest, argv);
+	close_err = close(src);
+	if (close_err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
+		exit(100);
+	}
 	close_err = close(dest);
 	if (close_err == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
 		exit(100);
 	}
-
 	return (0);
 }
