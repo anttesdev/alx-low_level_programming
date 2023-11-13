4cb48e33c41aa4047ac79ca7d381a0e63333a431
Anteneh <anttesdev>
2023-11-13 09:11:19 -0800

    all

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/anttesdev/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lotto.so
-export LD_PRELOAD=/tmp/lotto.so
+wget -P .. https://raw.githubusercontent.com/anttesdev/alx-low_level_programming/master/0x18-dynamic_libraries/lotto.so
+export LD_PRELOAD="/$PWD/../lotto.so"
