fb573f36f78e8fabfd6f610c1019d6b3faa1fdab
Anteneh <anttesdev>
2023-11-13 09:04:42 -0800

    all

--- 0x18-dynamic_libraries/lotto.c
+++ 0x18-dynamic_libraries/lotto.c
@@ -1,27 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <time.h>
+#include <unistd.h>
 
-void lotto(bool firstTime)
+int printf(const char *format, ...)
 {
-	int Numbers[] = {9, 8, 10, 24, 75, 9};
-	int i;
-
-	if (firstTime)
-	{
-		for (i = 0; i < sizeof(Numbers)/sizeof(Numbers[0]); i++)
-		{
-			printf("%d ", Numbers[i]);
-		}
-	}
-	else
-	{
-		for (int i = 0; i < 5; ++i)
-		{
-			printf("%d ", rand() % 75 + 1);
-		}
-		printf("%d\n", rand() % 15 + 1);
-	}
+	write(1, "9 8 10 24 75 - 9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
 }
-
