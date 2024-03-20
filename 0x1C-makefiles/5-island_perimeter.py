d4f35fbe7de0b89b3503a5f0719e478bf97dd1b7
Anteneh Tesfaye <anttesdev@gmail.com>
2024-03-21 01:35:26 +0300

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """
+    Args:
+    - grid (list of list of int): Represents the grid where 0 represents water and 1 represents land.
+
+    Returns:
+    - int: The perimeter of the island.
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
