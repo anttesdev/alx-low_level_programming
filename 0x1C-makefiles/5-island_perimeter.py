74f3edc34b271623dcc8891dcd999837191ca72d
Anteneh Tesfaye <anttesdev@gmail.com>
2024-03-21 01:45:25 +0300

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+A function to calculate the perimeter of an Island
+"""
+
+
 def island_perimeter(grid):
     """
     Args:
@@ -20,5 +25,4 @@ def island_perimeter(grid):
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
-
     return perimeter
